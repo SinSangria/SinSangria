712c66e032d8343141a70a743b8774837df8e1c2
SinSangria <166875375+SinSangria@users.noreply.github.com>
2024-04-21 03:10:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener('scroll', function() {
   const banner = document.querySelector('.floating-banner');
   const headerNavigation = document.querySelector('.header-navigation');
   if (window.pageYOffset > 100) {
-    banner.style.top = '-440px';
+    banner.style.top = '-140px';
     headerNavigation.style.top = '-180px';
   } else {
     banner.style.top = '0';
